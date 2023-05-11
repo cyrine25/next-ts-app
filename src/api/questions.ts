0ef95292bc2269920dc304c40c6a9f8b6ecec561
cyrine25 <amricyrine@gmail.com>
2023-05-11 10:36:00 +0200

    work in progress

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -6,7 +6,9 @@ interface QuestionDTO {
   answer: string
 }
 const fetchQuestions = async (): Promise<ReadonlyArray<Question>> => {
-  const data = await fetch('http://localhost:3000/data/questions.json')
+  const data = await fetch(
+    'https://raw.githubusercontent.com/cyrine25/next-ts-app/feat/api-poc/public/data/questions.json?token=GHSAT0AAAAAACCFMYWEVKGAHQUZGYZX2IYAZC4VEBA'
+  )
   const questions: ReadonlyArray<QuestionDTO> = await data.json()
   return questions.map(q => new Question(q.id, q.title, q.answer))
 }
