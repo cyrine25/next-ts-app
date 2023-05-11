266f95a593f639920e72e40f67ca5550f9d26f2b
cyrine25 <amricyrine@gmail.com>
2023-05-11 13:00:12 +0200

    work in progress

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -6,7 +6,7 @@ interface QuestionDTO {
   answer: string
 }
 const fetchQuestions = async (): Promise<ReadonlyArray<Question>> => {
-  const data = await fetch('http://127.0.0.1/data/questions.json')
+  const data = await fetch('http://127.0.0.1:3000/data/questions.json')
   const questions: ReadonlyArray<QuestionDTO> = await data.json()
   return questions.map(q => new Question(q.id, q.title, q.answer))
 }
