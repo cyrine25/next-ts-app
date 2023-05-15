0f1e87e30ef96e567eb4574fa04f09b4bdf61cd5
cyrine25 <amricyrine@gmail.com>
2023-05-15 15:37:27 +0200

    work in progress

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -1,15 +1,13 @@
 import { Question } from '@/domain/question'
 
-interface QuestionDTO {
-  id: number
-  title: string
-  answer: string
-}
-
 const fetchQuestions = async (): Promise<ReadonlyArray<Question>> => {
   const data = await fetch('http://localhost:5000/questions')
-  const questions: ReadonlyArray<QuestionDTO> = await data.json()
-  return questions.map(q => new Question(q.id, q.title, q.answer))
+  const questions: ReadonlyArray<Question> = await data.json()
+  return questions.map((q: Question) => ({
+    id: q.id,
+    title: q.title,
+    answer: q.answer,
+  }))
 }
 
 export default fetchQuestions
