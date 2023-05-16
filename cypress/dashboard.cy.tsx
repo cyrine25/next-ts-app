41dbd262b8f239530186229782e3e73a9649f8bd
cyrine25 <amricyrine@gmail.com>
2023-05-16 16:10:28 +0200

    work in progress

--- cypress/dashboard.cy.tsx
+++ cypress/dashboard.cy.tsx
@@ -1,3 +1,5 @@
+import React = require('react')
+
 import Dashboard from '@/app/dashboard/page'
 
 describe('<Dashboard />', () => {
--- cypress/questions.cy.tsx
+++ cypress/questions.cy.tsx
@@ -1,15 +1,15 @@
-import QuestionsCollapse from '@/components/questionsCollapse/QuestionsCollapse'
+import React = require('react')
 
+import QuestionsCollapse from '@/components/questionsCollapse/QuestionsCollapse'
+const questions = [
+  { id: 1, title: 'Question 1', answer: 'Answer 1' },
+  { id: 2, title: 'Question 2', answer: 'Answer 2' },
+  { id: 3, title: 'Question 3', answer: 'Answer 3' },
+  { id: 4, title: 'Question 4', answer: 'Answer 4' },
+]
 describe('QuestionsCollapse Component', () => {
   it('Should render the questions with answers', () => {
-    const questions = [
-      { id: 1, title: 'Question 1', answer: 'Answer 1' },
-      { id: 2, title: 'Question 2', answer: 'Answer 2' },
-      { id: 3, title: 'Question 3', answer: 'Answer 3' },
-    ]
-
     cy.intercept('GET', 'http://localhost:5000/questions', { body: questions })
-
     cy.mount(<QuestionsCollapse questions={questions} />)
     questions.forEach(question => {
       cy.get(`.ant-collapse-header-text:contains(${question.title})`).should('exist')
