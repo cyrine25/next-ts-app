4a0a093e4055ad88db073158aed9cc8ed5e20430
cyrine25 <amricyrine@gmail.com>
2023-09-20 14:37:24 +0200

    add api tracks

--- src/api/tracks.ts
+++ src/api/tracks.ts
@@ -1,7 +1,7 @@
 import { SeniorityLevel, Tracks } from '@/domain/tracks'
 
 const fetchTracks = async (): Promise<ReadonlyArray<Tracks>> => {
-  const data = await fetch('http://127.0.0.1:3000/data/tracks.json')
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_END_POINT}/tracks`)
   const tracks: ReadonlyArray<Tracks> = await data.json()
   return tracks.map((q: Tracks) => ({
     id: q.id,
